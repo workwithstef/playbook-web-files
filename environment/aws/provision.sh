8ee459fe026941ad4a5e2ff1b355082ed556c586
workwithstef <stefan.okolo@outlook.com>
2020-07-20 23:54:15 +0100

    test

--- environment/aws/provision.sh
+++
@@ -1,7 +0,0 @@
-
-# replace sshd_config file
-sudo rm /etc/ssh/sshd_config
-sudo cp /home/vagrant/environment/aws/sshd_config /etc/ssh/sshd_config
-sudo service ssh restart
-# changes password
-echo "root:vagrant" | sudo chpasswd
--- environment/db/provision.sh
+++
@@ -1,7 +0,0 @@
-
-# replace sshd_config file
-sudo rm /etc/ssh/sshd_config
-sudo cp /home/vagrant/environment/db/sshd_config /etc/ssh/sshd_config
-sudo service ssh restart
-# changes password
-echo "root:vagrant" | sudo chpasswd
